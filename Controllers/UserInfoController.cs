d335df1aafcc8d57ee351f8fa0242dc5e8ca0744
Nagendra94931 <nagendrababu.dotnet@gmail.com>
2023-02-22 22:05:59 +0530

    Initial commit

---
+++ Controllers/UserInfoController.cs
@@ -0,0 +1,137 @@
+﻿using Registration.DB;
+using Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Registration.Controllers
+{
+    public class UserInfoController : Controller
+    {
+
+        //DB Connection
+        MyDBEntities db = new MyDBEntities();
+
+
+        // GET: UserInfo
+        public ActionResult List()
+        {
+            var gg = db.NewUserregistrations.ToList();
+
+            return View(gg);
+        }
+
+
+
+        public ActionResult Create()
+        {
+            var model = new UserRegistrationvalid();
+
+
+            //Gender List Radiobutton-(Model)
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "Male", Value = "M" });
+            items.Add(new SelectListItem { Text = "Femal", Value = "F" });
+            items.Add(new SelectListItem { Text = "Other", Value = "O" });
+            model.GenderItems = items;
+            //ViewBag.GenderItems = items;
+
+
+
+
+
+            //Qualification List-(ViewBag)
+            List<SelectListItem> Qualificationitems = new List<SelectListItem>();
+            Qualificationitems.Add(new SelectListItem { Text = "PG", Value = "PG" });
+            Qualificationitems.Add(new SelectListItem { Text = "UG", Value = "UG" });
+            Qualificationitems.Add(new SelectListItem { Text = "Diploma", Value = "Diploma" });
+            //Assgin to ViewBag
+            ViewBag.Items = Qualificationitems;
+
+
+            //Course List-(Model)
+            List <SelectListItem> Courseitems = new List<SelectListItem>();
+            Courseitems.Add(new SelectListItem { Text = "Bcom", Value = "Bcom" });
+            Courseitems.Add(new SelectListItem { Text = "BA", Value = "BA" });
+            Courseitems.Add(new SelectListItem { Text = "Diploma", Value = "Diploma" });
+            //Assgin to Model
+            model.CourseItems = Courseitems;
+
+
+            //Get Countries from Database
+            ViewBag.country = new SelectList(db.Countries.ToList(), "CID", "CountryName");
+
+
+            //Get State from Database
+            ViewBag.state = new SelectList(db.States.ToList(), "SID","StateName");
+
+            return View(model);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Create(UserRegistrationvalid usr)
+        {
+            if(ModelState.IsValid)
+            {
+                NewUserregistration user = new NewUserregistration();
+
+                user.FirstName = usr.FirstName;
+                user.LastName = usr.LastName;
+                user.DOB = usr.DOB;
+                user.Gender = usr.Gender;
+                user.MarriageStatus = usr.MarriageStatus;
+
+                user.Qualification = usr.Qualification;
+                user.Course = usr.Course;
+                user.Language = usr.Language;
+                user.Country = usr.Country;
+                user.State = usr.State;
+                user.Address = usr.Address;
+                user.Mobile = usr.Mobile;
+                user.Email = usr.Email;
+                user.Password = usr.Password;
+
+                user.Status = true;
+                db.NewUserregistrations.Add(user);
+                db.SaveChanges();
+
+            }
+            return View("Create");
+        }
+
+
+
+
+        [HttpPost]
+        public JsonResult IsAlreadySigned(string Email)
+        {
+
+            var RegEmailId = (from u in db.NewUserregistrations
+                              where u.Email.ToUpper() == Email.ToUpper()
+                              select new { Email }).FirstOrDefault();
+
+            bool status;
+            if (RegEmailId != null)
+            {
+                //Already registered  
+                status = false;
+            }
+            else
+            {
+                //Available to use  
+                status = true;
+            }
+
+            return Json(status);
+
+        }
+
+
+    
+
+    }
+}
---
+++ Models/UserRegistrationvalid.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Registration.Models
+{
+    public partial class UserRegistrationvalid
+    {
+
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date of Birth")]
+        public string DOB { get; set; }
+
+
+        [Required]
+        [Display(Name = "Marriage Status")]
+        public string MarriageStatus { get; set; }
+
+        [Required]
+        public string Qualification { get; set; }
+
+        [Required]
+        public string Course { get; set; }
+        public List<SelectListItem> CourseItems { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        [Required]
+        public LanguageList LanguageItems { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Mobile { get; set; }
+
+        [Required]
+        [Remote("IsAlreadySigned", "UserInfo", HttpMethod = "POST", ErrorMessage = "EmailId already exists.")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm-Password")]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        public Nullable<bool> Status { get; set; }
+
+
+        //GenderItems
+        public List<SelectListItem> GenderItems { get; set; }
+
+        public bool IsCheck { get; set; }
+
+
+        [Required]
+        public IEnumerable<SelectListItem> Values
+        {
+            get
+            {
+                return new[]
+                {
+                new SelectListItem { Value = "1", Text = "Pre-Approved" },
+                new SelectListItem { Value = "2", Text = "Approved" },
+                };
+            }
+        }
+
+
+
+
+    }
+
+    public enum LanguageList
+    {
+        English=1,
+        Telugu=2
+    }
+
+
+    
+
+
+}
